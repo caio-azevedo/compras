99df9abf381ac673eea3132921908454ce3df8eb
Caio Azevedo <80236472+caio-azevedo@users.noreply.github.com>
2022-05-08 13:39:30 -0300

    Add files via upload

---
+++ auxiliar.py
@@ -0,0 +1,47 @@
+import time
+import math
+       
+item=int(input("Qual o número do outro item pesquisado?\n"))
+unidade = item % 10
+dezena = int(math.floor(item/10))% 10
+centena = int(math.floor(item/10))
+if item >= 100:
+    i = Cen[centena] + Dec[dezena] + Uni[unidade]
+else:
+    if item >= 10:
+        i = Dec[dezena] + Uni[unidade]
+    else:
+        i = Uni[item]
+    
+f1=3*(item-1)+1
+f2=f1+1
+f3=f1+2
+nomeitem=(input('Digite o nome do item.\n'))
+cot=int(input("Quantas cotações em sítios eletrônicos você fez para este item?\n"))
+pp=input("Você realizou a cotação deste item no painel de preço? Responda Sim ou Não.\n")
+print("Bom trabalho {}! Já estou juntando as informações todas!".format(nome))
+print("\n")
+print("Aguarde alguns segundos enquanto vou gerar o arquivo Latex para você!")
+print("\n")
+time.sleep(5)
+with open("Tex/item{}.txt".format(item), "w") as stream:
+    if cot==1 and pp=="Sim":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/pp%d}'%(i,nomeitem,i,f1,item),file=stream)
+    if cot==2 and pp=="Sim":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/pp%d}'%(i,nomeitem,i,f1,f2,item),file=stream)
+    if cot==3 and pp=="Sim":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/pp%d}'%(i,nomeitem,i,f1,f2,f3,item),file=stream)
+    if cot==1 and pp=="Não":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}'%(i,nomeitem,i,f1),file=stream)
+    if cot==2 and pp=="Não":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}'%(i,nomeitem,i,f1,f2),file=stream)
+    if cot==3 and pp=="Não":
+        print('\def \%sitem{%s}\n\includepdf[pages=1,pagecommand=\chapter{\%sitem}, offset=0 -2cm]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}\n\includepdf[pages=1]{Figuras/fig%d}'%(i,nomeitem,i,f1,f2,f3),file=stream)
+
+print("")
+resposta2=input("Você pesquisou mais algum item? Responda Sim ou Não.\n")
+if resposta2=="Não":
+    print("Ok então, obrigado vamos encerrar o programa, até a próxima!")
+    exit()
+if resposta2=="Sim":        
+    exec(open("auxiliar.py",encoding='utf8').read())
---
+++ print.py
@@ -0,0 +1,292 @@
+#pip install keyboard
+#pip install pyscreenshot
+
+import keyboard
+import pyscreenshot 
+
+x=1
+i=0
+with open("auxiliar_print.txt", "w") as stream:
+    while x <= 200:
+        print(f'contador{x} = {i}',file=stream)
+        x+=1
+        i+=3
+
+exec(open("auxiliar_print.txt").read())
+    
+while True:
+    if keyboard.is_pressed('a'):
+    
+        if keyboard.is_pressed('1'):
+            image = pyscreenshot.grab() 
+            contador1+=1
+            image.save('Figuras/fig{}.pdf'.format(contador1))
+            print ('Item 1')
+
+        if keyboard.is_pressed('2'):
+            image = pyscreenshot.grab() 
+            contador2+=1
+            image.save('Figuras/fig{}.pdf'.format(contador2))
+            print ('Item 2')
+
+        if keyboard.is_pressed('3'):
+            image = pyscreenshot.grab() 
+            contador3+=1
+            image.save('Figuras/fig{}.pdf'.format(contador3))
+            print ('Item 3')
+
+        if keyboard.is_pressed('4'):
+            image = pyscreenshot.grab() 
+            contador4+=1
+            image.save('Figuras/fig{}.pdf'.format(contador4))
+            print ('Item 4')
+
+        if keyboard.is_pressed('5'):
+            image = pyscreenshot.grab() 
+            contador5+=1
+            image.save('Figuras/fig{}.pdf'.format(contador5))
+            print ('Item 5')
+
+        if keyboard.is_pressed('6'):
+            image = pyscreenshot.grab() 
+            contador6+=1
+            image.save('Figuras/fig{}.pdf'.format(contador6))
+            print ('Item 6')
+
+        if keyboard.is_pressed('7'):
+            image = pyscreenshot.grab() 
+            contador7+=1
+            image.save('Figuras/fig{}.pdf'.format(contador7))
+            print ('Item 7')
+
+        if keyboard.is_pressed('8'):
+            image = pyscreenshot.grab() 
+            contador8+=1
+            image.save('Figuras/fig{}.pdf'.format(contador8))
+            print ('Item 8')
+
+        if keyboard.is_pressed('9'):
+            image = pyscreenshot.grab() 
+            contador9+=1
+            image.save('Figuras/fig{}.pdf'.format(contador9))
+            print ('Item 9')
+
+    if keyboard.is_pressed('b'):
+
+        if keyboard.is_pressed('0'):
+            image = pyscreenshot.grab() 
+            contador10+=1
+            image.save('Figuras/fig{}.pdf'.format(contador10))
+            print ('Item 10')
+    
+        if keyboard.is_pressed('1'):
+            image = pyscreenshot.grab() 
+            contador11+=1
+            image.save('Figuras/fig{}.pdf'.format(contador11))
+            print ('Item 11')
+
+        if keyboard.is_pressed('2'):
+            image = pyscreenshot.grab() 
+            contador12+=1
+            image.save('Figuras/fig{}.pdf'.format(contador12))
+            print ('Item 12')
+
+        if keyboard.is_pressed('3'):
+            image = pyscreenshot.grab() 
+            contador13+=1
+            image.save('Figuras/fig{}.pdf'.format(contador13))
+            print ('Item 13')
+
+        if keyboard.is_pressed('4'):
+            image = pyscreenshot.grab() 
+            contador14+=1
+            image.save('Figuras/fig{}.pdf'.format(contador14))
+            print ('Item 14')
+
+        if keyboard.is_pressed('5'):
+            image = pyscreenshot.grab() 
+            contador15+=1
+            image.save('Figuras/fig{}.pdf'.format(contador15))
+            print ('Item 15')
+
+        if keyboard.is_pressed('6'):
+            image = pyscreenshot.grab() 
+            contador16+=1
+            image.save('Figuras/fig{}.pdf'.format(contador16))
+            print ('Item 16')
+
+        if keyboard.is_pressed('7'):
+            image = pyscreenshot.grab() 
+            contador17+=1
+            image.save('Figuras/fig{}.pdf'.format(contador17))
+            print ('Item 17')
+
+        if keyboard.is_pressed('8'):
+            image = pyscreenshot.grab() 
+            contador18+=1
+            image.save('fig{}.pdf'.format(contador18))
+            print ('Item 18')
+
+        if keyboard.is_pressed('9'):
+            image = pyscreenshot.grab() 
+            contador19+=1
+            image.save('Figuras/fig{}.pdf'.format(contador19))
+            print ('Item 19')         
+
+    if keyboard.is_pressed('c'):
+
+        if keyboard.is_pressed('0'):
+            image = pyscreenshot.grab() 
+            contador20+=1
+            image.save('Figuras/fig{}.pdf'.format(contador20))
+            print ('Item 20')
+    
+        if keyboard.is_pressed('1'):
+            image = pyscreenshot.grab() 
+            contador21+=1
+            image.save('Figuras/fig{}.pdf'.format(contador21))
+            print ('Item 21')
+
+        if keyboard.is_pressed('2'):
+            image = pyscreenshot.grab() 
+            contador22+=1
+            image.save('Figuras/fig{}.pdf'.format(contador22))
+            print ('Item 22')
+
+        if keyboard.is_pressed('3'):
+            image = pyscreenshot.grab() 
+            contador23+=1
+            image.save('Figuras/fig{}.pdf'.format(contador23))
+            print ('Item 23')
+
+        if keyboard.is_pressed('4'):
+            image = pyscreenshot.grab() 
+            contador24+=1
+            image.save('Figuras/fig{}.pdf'.format(contador24))
+            print ('Item 24')
+
+        if keyboard.is_pressed('5'):
+            image = pyscreenshot.grab() 
+            contador25+=1
+            image.save('Figuras/fig{}.pdf'.format(contador25))
+            print ('Item 25')
+
+        if keyboard.is_pressed('6'):
+            image = pyscreenshot.grab() 
+            contador26+=1
+            image.save('Figuras/fig{}.pdf'.format(contador26))
+            print ('Item 26')
+
+        if keyboard.is_pressed('7'):
+            image = pyscreenshot.grab() 
+            contador27+=1
+            image.save('Figuras/fig{}.pdf'.format(contador27))
+            print ('Item 27')
+
+        if keyboard.is_pressed('8'):
+            image = pyscreenshot.grab() 
+            contador28+=1
+            image.save('Figuras/fig{}.pdf'.format(contador28))
+            print ('Item 28')
+
+        if keyboard.is_pressed('9'):
+            image = pyscreenshot.grab() 
+            contador29+=1
+            image.save('Figuras/fig{}.pdf'.format(contador29))
+            print ('Item 29')
+
+    if keyboard.is_pressed('d'):
+            
+        if keyboard.is_pressed('0'):
+            image = pyscreenshot.grab() 
+            contador30+=1
+            image.save('Figuras/fig{}.pdf'.format(contador30))
+            print ('Item 30')
+
+        if keyboard.is_pressed('1'):
+            image = pyscreenshot.grab() 
+            contador31+=1
+            image.save('Figuras/fig{}.pdf'.format(contador31))
+            print ('Item 31')
+
+        if keyboard.is_pressed('2'):
+            image = pyscreenshot.grab() 
+            contador32+=1
+            image.save('Figuras/fig{}.pdf'.format(contador32))
+            print ('Item 32')
+
+        if keyboard.is_pressed('3'):
+            image = pyscreenshot.grab() 
+            contador33+=1
+            image.save('Figuras/fig{}.pdf'.format(contador33))
+            print ('Item 33')
+
+        if keyboard.is_pressed('4'):
+            image = pyscreenshot.grab() 
+            contador34+=1
+            image.save('Figuras/fig{}.pdf'.format(contador34))
+            print ('Item 34')
+
+        if keyboard.is_pressed('5'):
+            image = pyscreenshot.grab() 
+            contador35+=1
+            image.save('Figuras/fig{}.pdf'.format(contador35))
+            print ('Item 35')
+
+        if keyboard.is_pressed('6'):
+            image = pyscreenshot.grab() 
+            contador36+=1
+            image.save('Figuras/fig{}.pdf'.format(contador36))
+            print ('Item 36')
+
+        if keyboard.is_pressed('7'):
+            image = pyscreenshot.grab() 
+            contador37+=1
+            image.save('Figuras/fig{}.pdf'.format(contador37))
+            print ('Item 37')
+
+        if keyboard.is_pressed('8'):
+            image = pyscreenshot.grab() 
+            contador38+=1
+            image.save('Figuras/fig{}.pdf'.format(contador38))
+            print ('Item 38')
+
+        if keyboard.is_pressed('9'):
+            image = pyscreenshot.grab() 
+            contador39+=1
+            image.save('Figuras/fig{}.pdf'.format(contador39))
+            print ('Item 39')
+
+    if keyboard.is_pressed('e'):
+            
+        if keyboard.is_pressed('0'):
+            image = pyscreenshot.grab() 
+            contador40+=1
+            image.save('Figuras/fig{}.pdf'.format(contador40))
+            print ('Item 40')
+
+        if keyboard.is_pressed('1'):
+            image = pyscreenshot.grab() 
+            contador41+=1
+            image.save('Figuras/fig{}.pdf'.format(contador41))
+            print ('Item 41')
+
+        if keyboard.is_pressed('2'):
+            image = pyscreenshot.grab() 
+            contador42+=1
+            image.save('Figuras/fig{}.pdf'.format(contador42))
+            print ('Item 42')
+
+        if keyboard.is_pressed('3'):
+            image = pyscreenshot.grab() 
+            contador43+=1
+            image.save('Figuras/fig{}.pdf'.format(contador43))
+            print ('Item 43')
+
+
+
+    if keyboard.is_pressed('z'):
+            
+        if keyboard.is_pressed('9'):
+            exit()
+
